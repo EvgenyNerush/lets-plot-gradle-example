1d242f4738d6e0bc3bbd6ac25adbbb6d2f28c095
Evgeny Nerush <nerush@appl.sci-nnov.ru>
2021-09-21 16:26:05 +0300

    a couple of plots; simpler build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,22 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+// файл build.gradle.kts
 
+// Этот плагин добавляет поддержку Котлина
 plugins {
     kotlin("jvm") version "1.4.32"
 }
 
-group = "me.eugn"
-version = "1.0-SNAPSHOT"
+// версия нашего проекта
+version = "1.1"
 
+// Используем известный репозиторий Java-проектов,
+// поддерживаемый Apache Software foundation
 repositories {
     mavenCentral()
 }
 
+// библиотеки из Maven Central,
+// функции из которых мы будем использовать в нашем коде
 dependencies {
-    testImplementation(kotlin("test-junit"))
     implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:3.0.2")
     implementation("org.jetbrains.lets-plot:lets-plot-image-export:2.1.0")
-}
-
-tasks.test {
-    useJUnit()
-}
-
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,37 @@
 import jetbrains.letsPlot.export.ggsave
 import jetbrains.letsPlot.geom.geomLine
 import jetbrains.letsPlot.geom.geomPoint
+import jetbrains.letsPlot.geom.geomSmooth
 import jetbrains.letsPlot.letsPlot
-import kotlin.random.Random
 
 fun main() {
-    val n = 5000
-    val gen = Random(12345) // seeded
-    val xs = arrayListOf(1.0, 2  , 3, 4)
-    val ys = arrayListOf(3.1, 1.7, 1, 2)
+    // определяем "облако" из точек
+    val xs = listOf(1, 2, 2, 3, 3, 3, 4, 5, 5, 5, 5)
+    val ys = listOf(8, 7, 4, 5, 4, 3, 4, 3, 2, 1, 2)
+    val zs = listOf(0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1)
 
-   val data = mapOf<String, Any>("x" to xs, "y" to ys)
-    val fig = letsPlot(data) + geomPoint(
-        color = "dark-green",
-        size = 2.0
-    ) { x = "x"; y = "y" }   + geomLine() { x = "x"; y = "y"}
+    // готовим данные для letsPlot в виду key-value хранилища
+    val data = mapOf<String, Any>("xvals" to xs, "yvals" to ys, "zvals" to zs)
 
-    ggsave(fig, "LetsPlotExample.png")
-    println("done!")
+    // график = данные + один слой + другой слой
+    val fig_1 = letsPlot(data) +
+            // здесь цвет - просто свойство точек
+            geomPoint( color = "dark-green"
+                     , size = 3.0
+                     ) { x = "xvals"; y = "yvals" } +
+            geomLine() { x = "xvals"; y = "yvals" }
+
+    // сохраняем в файл
+    ggsave(fig_1, "plot_1.png")
+
+    // ещё график
+    val fig_2 = letsPlot(data) +
+            // а здесь цвет - эстетика, он зависит от данных
+            geomPoint(size = 3.0) { x = "xvals"; y = "yvals"; color = "zvals"} +
+            // рисует усреднённые данные и разброс
+            geomSmooth() { x = "xvals"; y = "yvals"}
+
+    ggsave(fig_2, "plot_2.png")
+
+    println("bye!")
 }
